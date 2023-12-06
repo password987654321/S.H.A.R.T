974674d57b8917b9cfcfacb04f74093d15d24207
Marcus Feliciano <93349062+password987654321@users.noreply.github.com>
2023-12-06 16:12:36 -0500

    Small changes and pretty things.

--- start.ps1
+++ start.ps1
@@ -1,1 +1,2 @@
-docker build -t ace . -and docker run --name ace-run --rm --cap-add ALL --security-opt seccomp=unconfined --privileged ace
+#!/bin/pwsh
+docker build -t ace . ; if($?) {docker run --name ace-run --rm --cap-add ALL --security-opt seccomp=unconfined --privileged ace}
